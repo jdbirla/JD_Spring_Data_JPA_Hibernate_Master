8126f328eb93855d0518bb69ab9e86c07f5c0341
JDBIRLA <jitendra.birla21@gmail.com>
2023-01-27 13:45:36 +0530

    Adding jpa

---
+++ Java_Guide/documentation/postsdata.sql
@@ -0,0 +1,30 @@
+/*
+-- Query: SELECT * FROM myblog.posts
+LIMIT 0, 1000
+
+-- Date: 2022-12-16 07:45
+*/
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (1,'Follow the JDs path and become master in Java','Master in Java in 25 Days','Master in Java');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (2,'Follow the JDs path and become master in React','Master in React in 35 Days','Master in React');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (7,'Follow the JDs path and become master in Dbs like Oracle, MySQL, MongoDB','Master in DB in 25 Days','Master in DB');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (9,'Follow the JDs path and become master in microservices','Master in microservices in 25 Days','Master in microservices');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (10,'Follow the JDs path and become master in course1','course1','course1');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (11,'Follow the JDs path and become master in course2','course2','course2');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (12,'Follow the JDs path and become master in course3','course2','course3');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (13,'Follow the JDs path and become master in course4','course2','course4');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (14,'Follow the JDs path and become master in course5','course2','course5');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (15,'Follow the JDs path and become master in course6','course2','course6');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (16,'Follow the JDs path and become master in course7','course2','course7');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (17,'Follow the JDs path and become master in course8','course2','course8');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (18,'Follow the JDs path and become master in course9','course2','course9');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (19,'Follow the JDs path and become master in course10','course2','course10');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (20,'Follow the JDs path and become master in course11','course2','course11');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (21,'Follow the JDs path and become master in course12','course2','course12');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (22,'Follow the JDs path and become master in course13','course2','course13');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (23,'Follow the JDs path and become master in course14','course2','course14');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (24,'Follow the JDs path and become master in course15','course2','course15');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (25,'Follow the JDs path and become master in course16','course2','course16');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (26,'Follow the JDs path and become master in course17','course2','course17');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (27,'Follow the JDs path and become master in course18','course2','course18');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (28,'Follow the JDs path and become master in course19','course2','course19');
+INSERT INTO `` (`id`,`content`,`description`,`title`) VALUES (29,'Follow the JDs path and become master in course20','course2','course20');
